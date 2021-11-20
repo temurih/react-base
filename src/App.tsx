b49c3b5ac0164bb0a8c08e884414a452897ddf3a
Hafiz Temuri <temurih@live.co.uk>
2021-11-20 12:50:40 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import styled from 'styled-components';
+
+const AppStyle = styled.div`
+    text-align: center;
+
+    .app-logo {
+        height: 40vmin;
+        pointer-events: none;
+    }
+
+    @media (prefers-reduced-motion: no-preference) {
+        .app-logo {
+            animation: app-logo-spin infinite 20s linear;
+        }
+    }
+
+    .app-header {
+        background-color: #282c34;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+        color: white;
+    }
+
+    .app-link {
+        color: #61dafb;
+        transition: color 1s;
+    }
+    .app-link:hover {
+        color: #fff;
+    }
+
+    @keyframes app-logo-spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`;
+
+const App = () => {
+    return (
+        <AppStyle>
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="app-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </AppStyle>
+    );
+};
+
+export default App;
