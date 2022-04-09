cf67c9a8d042a0c43cf4dd37456dcae73489c5ab
Hafiz Temuri <temurih@live.co.uk>
2022-04-09 10:26:26 -0600

    removing error thrown, instead a console.log

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -4,7 +4,7 @@ import applyCaseMiddleware from 'axios-case-converter';
 const apiUrl = process.env.REACT_APP_API_URL_BASE;
 
 if (!apiUrl) {
-    throw new Error(
+    console.log(
         `Please create .env file in the root folder of the app and define REACT_APP_API_BASE_URL=https://some-backend-url.com`,
     );
 }
